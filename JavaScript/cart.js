ab95a797c82db08d1e5bc50425d35eaf54e5ca0e
code-aniruddha <GANGADHARCHAUDHARI1970@GMAIL.COM>
2024-12-31 08:35:05 +0530

    Total amount added

--- JavaScript/cart.js
+++ JavaScript/cart.js
@@ -1,8 +1,8 @@
-
-let total = 0;
+let total;
 let cart = JSON.parse(localStorage.getItem('cart'));
   display();
     function display(){
+      total = 0;
       let displayElement = document.querySelector('.list');
       let newHtml =`
           <div class="list-items">
@@ -14,20 +14,21 @@ let cart = JSON.parse(localStorage.getItem('cart'));
           </div>  
         `;
       for(let i = 0;i < cart.length;i++){
+        total += cart[i].price * cart[i].quantity;
         newHtml += `
           <div class="list-items">
             <span>${i + 1}. ${cart[i].name}</span>
             <span>${cart[i].price}</span>
              <span>${cart[i].quantity}</span>
              <span> ${eval(cart[i].quantity * cart[i].price)} </span>
-            <button onclick="cart.splice(${i},1);
-             localStorage.setItem('cart', JSON.stringify(cart));;
+            <button onclick="cart.splice(${i},1);           
+            localStorage.setItem('cart', JSON.stringify(cart));
             display();"><img src="delete.png"></button>
-          </div>  
+          </div>
         `;
-        total += cart[i].price * cart[i].quantity;
       }
-      displayElement.innerHTML = newHtml + `<h1>Grand Total :${total}</h1>`;
+      displayElement.innerHTML = newHtml + `<h1>Grand Total : ${total}</h1>`;
+      localStorage.setItem('price',total);
     }
     function goToPay(){
       var link = document.getElementById('next');
